e3a7c7e2e9484b3a16d8d2652526daf3575a0343
Donnn2 <kdowon92@naver.com>
2022-03-19 16:08:50 +0900

    투두헤더 추가 완료

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import GlobalStyle from "./styles/GlobalStyle";
 import { RecoilRoot } from 'recoil'
 import TodoTemplate from "./components/TodoTemplate";
 import TodoList from "./components/TodoList";
+import TodoHeader from "./components/TodoHeader";
 
 function App() {
   return (
     <RecoilRoot>
       <GlobalStyle />
       <TodoTemplate>
+        <TodoHeader />
         <TodoList />
       </TodoTemplate>
     </RecoilRoot>
